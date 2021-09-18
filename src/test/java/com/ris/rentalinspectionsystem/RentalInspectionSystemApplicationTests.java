89c232cca8f17d509dfa41e7ebc1cdc08cc43e9b
Henry Huang <henry.h@wbstech.com.au>
2021-09-18 21:34:27 +1000

    RIS-1: Import spring project

---
+++ src/test/java/com/ris/rentalinspectionsystem/RentalInspectionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ris.rentalinspectionsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalInspectionSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
