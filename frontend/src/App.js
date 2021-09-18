cc461cf8b2aadefc9024398fed4c359f6749f5b4
Kenneth Lu <ken.zc.lu@gmail.com>
2021-09-18 23:51:53 +1000

    RIS-4: Added styled-components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Table = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  padding: 16px;
+
+  text-align: center;
+
+  height: 100%;
+  width: 100%;
+`
+
+const TableHeader = styled.h1`
+`
+
+const TableContent = styled.div`
+  /* margin: 8px 0 0 0; */
+`
 
 function App() {
+  const [data, setData] = useState([{
+    id: 1,
+    username: "Mike",
+    password: "blah"
+  }, {
+    id: 2,
+    username: "Mike",
+    password: "blah"
+  }, {
+    id: 3,
+    username: "Mike",
+    password: "blah"
+  }]);
+
+  useEffect(async () => {
+    const result = await axios.get("http://localhost:8080/api/agent");
+    setData(result);
+  }, []);
+
+  // fetch("/api/agent")
+  //     .then(response => response.json())
+  //     .then(agents => {
+  //         for (const agent of agents) {
+  //             let row = newBody.insertRow();
+  //             row.insertCell(0).appendChild(document.createTextNode(`${agent.id}`))
+  //             row.insertCell(1).appendChild(document.createTextNode(`${agent.username}`))
+  //             row.insertCell(2).appendChild(document.createTextNode(`${agent.password}`))
+  //         }
+  //     })
+
+  // table.replaceChild(newBody, oldBody)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {!data.length && <div>Loading...</div>}
+      {!!data.length && <Table>
+        <TableHeader>ID</TableHeader>
+        <TableHeader>Username</TableHeader>
+        <TableHeader>Password</TableHeader>
+        {data.map(d => <>
+          <TableContent key={d.id}>
+            {`${d.id}`}
+          </TableContent>
+          <TableContent key={d.id}>
+            {`${d.username}`}
+          </TableContent>
+          <TableContent key={d.id}>
+            {`${d.password}`}
+          </TableContent>
+        </>)}
+      </Table>}
+    </>
   );
 }
 
